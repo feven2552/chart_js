3d7e51010c2410eaaa8080ce7b97a4c905430a36
feven <93426602+feven2552@users.noreply.github.com>
2023-07-15 03:28:49 +0300

    Add files via upload

---
+++ chart1.js
@@ -0,0 +1,20 @@
+const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Ethiopia', 'china', 'usa', 'norway', 'Alabania', 'Sweden'],
+    datasets: [{
+      label: '300',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
---
+++ chart2.js
@@ -0,0 +1,48 @@
+const ctx2= document.getElementById('doughnut');
+
+new Chart(ctx2, {
+  type: 'doughnut',
+  data: {
+    labels: ['Ethiopia', 'china', 'usa', 'norway', 'Alabania', 'Sweden'],
+    datasets: [{
+      label: '300',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    
+      
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+const config = {
+    type: 'doughnut',
+    data: data,
+  };
+  
+  const data = {
+    labels: [
+      'Red',
+      'Blue',
+      'Yellow'
+    ],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [300, 50, 100],
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 205, 86)'
+      ],
+      hoverOffset: 4
+    }]
+  };
+  
+  
+  
