5b9a54bcaeba2762a6522735ee959858af9e192e
feven <feven.25yi@gmail.com>
2024-02-07 18:09:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+
+let person = {
+ name: 'feven',
+ age: 23,
+ gender:'undefined',
+}
+
+//dot notation
+person.gender = 'female'
+person.age= 24;
+
+console.log(person);
+
+//bracket notation
+person['name'] = 'Eyosi';
+person['gender'] = 'male'
+person['age'] = 12;
+
+console.log(person);
+
+
+
+let selectedcolors = ['red', 'pink'];
+selectedcolors[1]= 'black';
+console.log(selectedcolors.length);
+
+
+
+function greet(name){
+    console.log('hello' + " " + name);
+}
+
+greet('feven');
+greet("eyosi");
+
+
+
