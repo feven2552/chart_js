814c903f0abd1c2277ee53449350c2b4dc40beba
feven <feven.25yi@gmail.com>
2024-02-10 20:29:28 +0300

    Update chart1.js

--- chart1.js
+++ chart1.js
@@ -1,15 +1,15 @@
-// const ctx = document.getElementById('myChart');
+const ctx = document.getElementById('myChart');
 
-// new Chart(ctx, {
-//   type: 'bar',
-//   data: {
-//     labels: ['Ethiopia', 'china', 'usa', 'norway', 'Alabania', 'Sweden'],
-//     datasets: [{
-//       label: '300',
-//       data: [12, 19, 3, 5, 2, 3],
-//       borderWidth: 1
-//     }]
-//   },
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Ethiopia', 'china', 'usa', 'norway', 'Alabania', 'Sweden'],
+    datasets: [{
+      label: '300',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
   options: {
     scales: {
       y: {
