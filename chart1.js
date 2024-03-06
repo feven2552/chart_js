d6a2b61f8c4e2bfcb45ac7e6f8892e31055b0ada
Feven <feven.25yi@gmail.com>
2024-03-06 23:36:43 +0300

    Update chart1.js

--- chart1.js
+++ chart1.js
@@ -18,3 +18,54 @@ new Chart(ctx, {
     }
   }
 });
+const ctx4= document.getElementById('scatterchart');
+
+new Chart(ctx4, {
+  type: 'scatterchart',
+  data: {
+    labels: ['Ethiopia', 'china', 'usa', 'norway', 'Alabania', 'Sweden'],
+    datasets: [{
+      label: '300',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+
+
+const ctx4= document.getElementById('scatterchart');
+
+new Chart(ctx4, {
+  type: 'scatterchart',
+  data: {
+    labels: ['Ethiopia', 'china', 'usa', 'norway', 'Alabania', 'Sweden'],
+    datasets: [{
+      label: '300',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+      
+    }]
+  },
+});
+
+const labels = Utils.months({count: 7});
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'My First Dataset',
+    data: [65, 59, 80, 81, 56, 55, 40],
+    fill: false,
+    borderColor: 'rgb(75, 192, 192)',
+    tension: 0.1
+  }]
+};
+
+options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+
+const config = {
+    type: 'line',
+    data: data,
+  };
